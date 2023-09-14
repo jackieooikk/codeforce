de186fc9e410e2e2b8fdd8c0f5e1a221d5f5e06d
jackieooikk <98008869+jackieooikk@users.noreply.github.com>
2023-09-14 10:58:08 +1000

    fixed doublestrings

--- 1100/doublestrings.cpp
+++ 1100/doublestrings.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <set>
 using namespace std;
 
 int main() {
@@ -11,36 +12,34 @@ int main() {
         cin >> n;
 
         string letters[n];
-
+        set<string> s;
         for (int i = 0; i < n; i++) {
             cin >> letters[i];
+            s.insert(letters[i]);
         }
 
         for (int i = 0; i < n; i++) {
             string primary = letters[i];
-            int match = 0;
-            for (int j = 0; j < n; j++) {
-                string first = letters[j];
-                for (int k = 0; k < n; k++) {
-                    string second = letters[k];
-                    if (i != j && i != k && primary.length() == first.length() + second.length()) {
-                        if (primary == first + second) {
-                            match = 1;
-                        }
-                    }
-                }
-                if (match) {
+
+            int size = primary.size();
+
+            int contains = 0;
+            
+            for (int j = 1; j < size; j++) {
+                if (s.count(primary.substr(0,j)) == 1 && s.count(primary.substr(j, size)) == 1) {
+                    contains = 1;
                     break;
                 }
             }
-            if (match) {
+            if (contains) {
                 cout << 1;
             } else {
                 cout << 0;
             }
         }
-
         cout << endl;
+
+        
     }
 }
 
