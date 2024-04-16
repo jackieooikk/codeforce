060f3b5d04e10c3483ddbb304117173a6725d288
jackieooikk <98008869+jackieooikk@users.noreply.github.com>
2024-04-16 13:27:27 +0800

    dp - sorting array and using upper bound

---
+++ 1300/queries.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include <algorithm>
+using namespace std;
+typedef long long int ll;
+int main()
+{
+        ll a,b;
+        ll i,j,k;
+
+        cin>>a>>b;
+        ll x[a],y[b];
+
+        for(i=0;i<a;i++)
+            cin>>x[i];
+
+        for(i=0;i<b;i++)
+            cin>>y[i];
+
+        sort(x,x+a);
+
+        for(i=0;i<b;i++)
+        {
+            cout<<upper_bound(x,x+a,y[i])-x<<" ";
+        }
+
+        return 0;
+}
